4be9406b6de1573b04cf2ebc717233bf96bf72c3
profesor79 <profesor79@gmail.com>
2015-11-18 14:11:26 +0000

    Added first project

---
+++ profesor79.LocalLibraby/profesor79.LocalLibraby.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="profesor79.LocalLibraby.Web.MvcApplication" Language="C#" %>
---
+++ profesor79.LocalLibraby/profesor79.LocalLibraby.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(profesor79.LocalLibraby.Web.Startup))]
+namespace profesor79.LocalLibraby.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

3eefa745e86e414798913558237bf7fcbc95626f
profesor79 <profesor79@gmail.com>
2015-11-19 13:40:07 +0000

    added next projects

---
+++ profesor79.LocalLibrary/profesor79.LocalLibrary.DtoObjects/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace profesor79.LocalLibrary.DtoObjects
+{
+    public class Class1
+    {
+    }
+}
